65fe92c2489c4a87a806d68dad09d824c67b4c2c
Elbek Rustamjanov <elbek4561@gmail.com>
2026-08-15 18:11:27 +0500

    added templates

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import {DatabaseModule} from "./database/database.module";
 import {ConfigModule} from "@nestjs/config";
+import { TemplatesModule } from './templates/templates.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }),DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),DatabaseModule, TemplatesModule],
   controllers: [],
   providers: [],
 })
